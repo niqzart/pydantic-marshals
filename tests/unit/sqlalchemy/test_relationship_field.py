32784da5358a79f57c3b8ffa7110957915a3aeb6
niqzart <qwert45hi@yandex.ru>
2023-09-24 22:06:09 +0300

    feat: testing relationship field

---
+++ tests/unit/sqlalchemy/test_relationship_field.py
@@ -0,0 +1,103 @@
+from typing import Any, get_args, get_origin
+from unittest.mock import Mock
+
+import pytest
+from pydantic import BaseModel
+from sqlalchemy.orm import Relationship
+
+from pydantic_marshals.sqlalchemy.fields.relationships import RelationshipField
+from tests.unit.conftest import DummyFactory
+
+
+def test_conversion() -> None:
+    relationship_dummy: Relationship[Any] = Relationship()
+    model_dummy = BaseModel
+
+    column_field = RelationshipField.convert(relationship_dummy, model_dummy)
+    assert isinstance(column_field, RelationshipField)
+    assert column_field.relationship is relationship_dummy
+    assert column_field.model is model_dummy
+
+
+@pytest.mark.parametrize(
+    "source",
+    [
+        pytest.param((int,), id="single_random_argument"),
+        pytest.param((int, int), id="double_random_argument"),
+        pytest.param((Relationship(),), id="only_relationship"),
+        pytest.param((int, BaseModel), id="only_model"),
+    ],
+)
+def test_conversion_fails(source: tuple[Any] | tuple[Any, Any]) -> None:
+    assert RelationshipField.convert(*source) is None
+
+
+@pytest.fixture()
+def mapped_relationship_mock() -> Mock:
+    return Mock()
+
+
+@pytest.fixture()
+def relationship_field(
+    dummy_factory: DummyFactory,
+    mapped_relationship_mock: Mock,
+) -> RelationshipField:
+    return RelationshipField(mapped_relationship_mock, dummy_factory("model"))
+
+
+def test_name_generation(
+    dummy_factory: DummyFactory,
+    relationship_field: RelationshipField,
+    mapped_relationship_mock: Mock,
+) -> None:
+    mapped_relationship_mock.key = dummy_factory("return")
+    assert relationship_field.generate_name() is dummy_factory("return")
+
+
+def test_type_generation(
+    dummy_factory: DummyFactory,
+    relationship_field: RelationshipField,
+    mapped_relationship_mock: Mock,
+) -> None:
+    mapped_relationship_mock.collection_class = None
+    assert relationship_field.generate_type() is dummy_factory("model")
+
+
+def test_type_generation_list(
+    dummy_factory: DummyFactory,
+    relationship_field: RelationshipField,
+    mapped_relationship_mock: Mock,
+) -> None:
+    mapped_relationship_mock.collection_class = list
+
+    real = relationship_field.generate_type()
+    assert get_origin(real) is list
+    assert len(get_args(real)) == 1
+    assert get_args(real)[0] is dummy_factory("model")
+
+
+@pytest.mark.parametrize(
+    ("collection_type", "error_message"),
+    [
+        pytest.param(3, "Collection is not a type: 3", id="not_a_type"),
+        pytest.param(dict, "Bad collection class: <class 'dict'>", id="bad_class"),
+    ],
+)
+def test_type_generation_collection_error(
+    dummy_factory: DummyFactory,
+    relationship_field: RelationshipField,
+    mapped_relationship_mock: Mock,
+    collection_type: Any,
+    error_message: str,
+) -> None:
+    mapped_relationship_mock.collection_class = collection_type
+
+    with pytest.raises(RuntimeError) as exc:
+        relationship_field.generate_type()
+
+    assert len(exc.value.args) == 1
+    assert exc.value.args[0] == error_message
+
+
+def test_field_data_generation(relationship_field: RelationshipField) -> None:
+    assert dict(relationship_field.generate_field_data()) == {}
