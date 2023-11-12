bdd40678d26040afed96636f56c2360badfcc4ed
niqzart <qwert45hi@yandex.ru>
2023-11-12 23:25:08 +0300

    fix: replace patch default with singleton enum

--- pydantic_marshals/base/__init__.py
+++ pydantic_marshals/base/__init__.py
@@ -1,3 +1,4 @@
+from pydantic_marshals.base.fields.base import PatchDefault, PatchDefaultType
 from pydantic_marshals.base.models import MarshalBaseModel, MarshalModel
 
-__all__ = ("MarshalBaseModel", "MarshalModel")
+__all__ = ("MarshalBaseModel", "MarshalModel", "PatchDefault", "PatchDefaultType")
